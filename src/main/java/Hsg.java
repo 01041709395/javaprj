8eb79ad0c202066cdc96a8fec00ddabd1c0666da
01041709395 <gasu9395@naver.com>
2024-03-11 18:19:36 +0900

    2024

---
+++ src/main/java/Hsg.java
@@ -0,0 +1,11 @@
+public class Hsg {
+    public static void main(String[] args) {
+
+        int a, b, result;
+        a = 500;
+        b = 1000;
+
+        result = a-b;
+        System.out.println(result);
+    }
+}
