6cc45ee51bb9dc9d8bbf07103fe51d81e3995273
01041709395 <gasu9395@naver.com>
2024-03-13 19:56:30 +0900

    2024

---
+++ src/main/java/Add1.java
@@ -0,0 +1,12 @@
+public class Add1 {
+    public static void main (String[] args) {
+
+        int a,b,c,d;
+
+        System.out.println("첫 번째 계산할 값을 입력하세요 ==> ");
+        System.out.println("두 번째 계산할 값을 입력하세요 ==> ");
+        System.out.println("세 번째 계산할 값을 입력하세요 ==> ");
+        System.out.println("네 번째 계산할 값을 입력하세요 ==> ");
+
+    }
+}
