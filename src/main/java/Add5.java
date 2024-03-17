286f7ff4d374063ace076f36b1efb72180ae197f
01041709395 <gasu9395@naver.com>
2024-03-17 16:09:28 +0900

    2024

---
+++ src/main/java/Add5.java
@@ -0,0 +1,23 @@
+public class Add5 {
+    public static void main (String[] args) {
+
+
+        int num = 20;
+        num++;
+        System.out.println(num+ "");
+        num--;
+        System.out.println(num+ "");
+        num += 3 ;
+        System.out.println(num+  "");
+        num -= 3;
+        System.out.println(num+ "");
+        num *= 3 ;
+        System.out.println(num+ "");
+        num /= 3 ;
+        System.out.println(num+ "");
+        num %= 3 ;
+        System.out.println(num+ "");
+
+
+    }
+}
