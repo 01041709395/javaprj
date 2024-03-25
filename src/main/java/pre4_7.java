9d60c79db1916f3360c0c83e703eb5828ae92f53
01041709395 <gasu9395@naver.com>
2024-03-25 20:04:02 +0900

    25.03

---
+++ src/main/java/pre4_7.java
@@ -0,0 +1,16 @@
+public class pre4_7 {
+    public static void main(String[] args) {
+        String var1 = "작은따옴표는 \' 모양입니다.";
+        String var2 = "큰따옴표는 \" 모양입니다.";
+        String var3 = "난생처음 \n" +
+                      "자바를 \n" +
+                      "열공중입니다. ";
+
+
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+
+    }
+}
