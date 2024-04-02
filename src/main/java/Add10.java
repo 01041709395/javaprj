36176bee34be12783f3f70f3436f6cf0f3df0855
01041709395 <gasu9395@naver.com>
2024-04-02 15:18:59 +0900

    02.04

---
+++ src/main/java/Add10.java
@@ -0,0 +1,9 @@
+public class Add10 {
+    public static void main(String[] args) {
+        int n1 = 100, n2 = 200;
+
+        System.out.print(n1 == n2);
+        System.out.print("");
+        System.out.println(n1 != n2);
+    }
+}
