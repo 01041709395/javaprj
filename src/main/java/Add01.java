bf063d0c1a67c88f17986e67aee04aa11ab5e05f
01041709395 <gasu9395@naver.com>
2024-04-09 08:51:36 +0900

    09.04

---
+++ src/main/java/Add01.java
@@ -0,0 +1,22 @@
+import java. util. Scanner;
+public class Add01 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String str;
+
+        System.out.print("문자열 입력 ==> ");
+        str = s.nextLine();
+
+        System.out.print("출력 문자열 ==> ");
+        for(int i=0; i<str.length(); i++) {
+            if (str.charAt(i) == 'O')
+                System.out.printf("%c", '@');
+            else
+                System.out.printf("%C", str.charAt(i));
+        }
+
+
+
+    }
+}
--- src/main/java/add15.java
+++ src/main/java/add15.java
@@ -1,22 +1,50 @@
-import java. util. Scanner;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class add15 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
-        int a,b;
-        int c,d,e,f;
 
+        int a, b;
+        int result;
+        char k;
+
+        System.out.print("첫번째 수를 입력하세요 ==> ");
         a = s.nextInt();
+        System.out.print("+-*/% ==> ");
+        k = (char) System.in.read();
+        System.out.print("두번째 수를 입력하세요 ==> ");
         b = s.nextInt();
-        c = a*b;
-        d = a+b;
-        e = a-b;
-        f = a/b;
-        System.out.println(a+ "*" +b+ "=" +c);
-        System.out.println(a+ "+" +b+ "=" +d);
-        System.out.println(a+ "-" +b+ "=" +e);
-        System.out.println(a+ "/" +b+ "=" +f);
 
-        s.close();
-
+        if (k == '+') {
+            result = a + b;
+            System.out.println(a + "+" + b + "=" + result);
+        }
+        if (k == '-') {
+            result = a - b;
+            System.out.println(a + "-" + b + "=" + result);
+        }
+        if (k == '*') {
+            result = a * b;
+            System.out.println(a + "*" + b + "=" + result);
+        }
+        if (k == '/') {
+            if (b != 0) {
+                result = a / b;
+                System.out.println(a + "/" + b + "=" + result);
+            } else {
+                System.out.println("0으로 나누면 안됩니다.");
+            }
+            if (k == '%') {
+                if (b != 0) {
+                    result = a % b;
+                    System.out.println(a + "%" + b + "=" + result);
+                } else {
+                    System.out.println("0으로 나누면 나머지 값이 안됩니다.");
+                }
+                s.close();
+            }
+        }
     }
 }
+
